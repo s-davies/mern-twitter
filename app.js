0b48150c8baacb01c624473dcd674095dd937fdd
s-davies <swdavies.contact@gmail.com>
2020-03-15 23:55:36 -0700

    finished mern twitter

--- app.js
+++ app.js
@@ -2,15 +2,26 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const passport = require('passport');
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser')
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hi World"));
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
